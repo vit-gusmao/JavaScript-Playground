c7379205af29ebb77b7cf6e7e334be241b69af81
vit-gusmao <vitorgusmao1808@gmail.com>
2026-08-14 19:24:12 -0300

    Add examples for null and typeof, and enhance existing examples in Types of Value section

---
+++ Variables/Types of Value/script.js
@@ -0,0 +1,37 @@
+// String Example
+let name = "Maria";
+
+console.log("Welcome, ${name}"); // Welcome, Maria
+
+// Number Example
+let age = 25;
+let price = 9.99;
+let num = 2e10;
+
+console.log(age + price); // 34.99
+console.log(num); // 20000000000
+
+// Boolean Example
+let isLoggedIn = true;
+let isAdult = false;
+
+console.log(isLoggedIn); // true
+console.log(isAdult); // false
+
+// Undefined Example
+let result;
+
+console.log(result); // undefined
+
+// Null Example
+let shoppingCartPrice = null;
+console.log(shoppingCartPrice); // null
+
+// Typeof Example
+let age30 = 30;
+let nameAnna = "Anna";
+let isStudent = false;
+
+console.log(typeof age30); // "number"
+console.log(typeof nameAnna); // "string"
+console.log(typeof isStudent); // "boolean"
