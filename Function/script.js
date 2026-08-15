1e3d5e86b83d0dfe41a39a4fb24e45cbc8d18e70
vit-gusmao <vitorgusmao1808@gmail.com>
2026-08-15 10:24:21 -0300

    Add new caller function to script.js with multiple greeting examples

--- Function/script.js
+++ Function/script.js
@@ -3,3 +3,11 @@ function welcoming(){
     }
     
     welcome(); // "welcome"
+
+    function caller(message, name){
+        console.log(message, name);
+        }
+        
+        caller("Hello", "André"); // Hello André 
+        caller("Good Morning", "Paulo"); // Good Morning Paulo 
+        caller("Good Night", "Zoe"); // Good Night Zoe
