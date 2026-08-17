ebe344c0ef396448df68569d9856846bf88443e0
vit-gusmao <vitorgusmao1808@gmail.com>
2026-08-17 16:25:02 -0300

    Enhance Function section in index.html with a new 'Return' article and update examples; refactor script.js to improve function definitions and add a new callerNick function for personalized greetings. Update CSS for better code block styling.

--- Function/script.js
+++ Function/script.js
@@ -1,13 +1,20 @@
-function welcoming(){
-    Console.log("welcome");
-    }
-    
-    welcome(); // "welcome"
+function welcome() {
+    console.log("Welcome");
+}
 
-    function caller(message, name) {
-        console.log(message, name);
-        }
-        
-        caller("Hello", "André"); // Hello André 
-        caller("Good Morning", "Paulo"); // Good Morning Paulo 
-        caller("Good Night", "Zoe"); // Good Night Zoe
+function caller(message, name) {
+    console.log(`${message} ${name}`);
+}
+
+function callerNick(nickname) {
+    return `Welcome, ${nickname}`;
+}
+
+// Example usages:
+welcome(); // "Welcome"
+
+caller("Hello", "André"); // Hello André 
+caller("Good Morning", "Paulo"); // Good Morning Paulo 
+caller("Good Night", "Zoe"); // Good Night Zoe
+
+console.log(callerNick("André")); // Welcome, André
