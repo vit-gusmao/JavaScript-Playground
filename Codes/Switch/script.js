72ba702df28cd12950869ddbd0413e9163107247
vit-gusmao <vitorgusmao1808@gmail.com>
2026-08-19 22:09:10 -0300

    update

---
+++ Codes/Switch/script.js
@@ -0,0 +1,17 @@
+let select = 2;
+
+switch (select) {
+    
+    case 1:
+    console.log("number 1 is selected");
+    break;
+    
+    case 2:
+    console.log("number 2 is selected");
+    break;
+    
+    default:
+    console.log("invalid option");
+    break;
+}
+// you chose number 2
